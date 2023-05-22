531670c0749235fd814d4e130774ee065e176def
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-21 21:11:23 -0300

    task0

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+aaaaaa
+"""
+
+
+def pascal_triangle(n):
+    """
+    aaaaa
+    """
+    pascal_list = []
+    if n <= 0:
+        return pascal_list
+    for PEPE1 in range(n):
+        row = [1]
+        if pascal_list:
+            last_row = pascal_list[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        pascal_list.append(row)
+    return pascal_list
