5bcf52d898c0e99f158603ea6abc3f0250eff23f
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-29 17:34:17 -0300

    task0

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+aaaa
+"""
+
+
+def canUnlockAll(boxes):
+    """
+
+    """
+    myKeys = [0]
+    for key in myKeys:
+        for boxKey in boxes[key]:
+            if boxKey not in myKeys and boxKey < len(boxes):
+                myKeys.append(boxKey)
+    if len(myKeys) == len(boxes):
+        return True
+    return False
