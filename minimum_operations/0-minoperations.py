b823c6a20329d5e8dcd6b4c11147a96fedf28e03
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-29 17:44:00 -0300

    task0

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+    num = n
+    div = 2
+    operations = 0
+
+    while num > 1:
+        if num % div == 0:
+            num = num / div
+            operations = operations + div
+        else:
+            div += 1
+    return operations
